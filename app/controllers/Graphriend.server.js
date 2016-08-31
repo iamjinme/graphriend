e2d10c8dc99c93f36b8665027f04f475cae142b1
Jinme Mirabal <mirabalj@gmail.com>
2016-08-30 21:02:20 -0400

    Create Sign up controllers and connect with frontend

--- app/controllers/Graphriend.server.js
+++ app/controllers/Graphriend.server.js
@@ -5,6 +5,40 @@ var sess;
 
 function Graphriend () {
 
+  // Sign Up
+  this.signUp = function(req, res) {
+		sess = req.session;
+		if (!req.body.name) {
+			res.json({error: true, message: 'What is your name?'});
+		} else if (!req.body.username) {
+			res.json({error: true, message: 'Username is necessary'});
+		} else if (!req.body.password) {
+			res.json({error: true, message: 'Password its empty, fill this'});
+		} else {
+			// var user = { 'name': req.body.name, 'email': req.body.username, 'password': req.body.password, 'date': new Date() };
+			Users.findOne({ 'username': req.body.username }, function(err, doc) {
+				if (err) throw err;
+      	if (doc) {
+					// User is registered
+					res.json({error: true, message: 'Friend registered with this username'});
+				} else {
+					// Save new user
+					var user = new Users({
+						'name': req.body.name,
+						'username': req.body.username,
+					  'password': req.body.password,
+					  'date': new Date()
+					});
+					user.save(function(err, doc) {
+        		if(err) throw err;
+        		res.json(doc);
+      		});
+				};
+			});
+		};
+  };
+
+
 }
 
 module.exports = Graphriend;
--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,43 @@
 $(document).ready(function() {
   // Listen events
   $('#btn_signup').click(function() {
-    // Sign Up
+    // Show Form Sign Up
     console.log('Sign Up');
     $('#login_modal').addClass('active');
     $('#login_title').html('Sign Up');
     $('#submit_signup').removeClass('hide');
     $('#submit_login').addClass('hide');
     $('#name_input').removeClass('hide');
+    $('#login_message_container').addClass('hide');
   });
   $('#btn_login').click(function() {
-    // Sign Up
+    // Show Form Log In
     console.log('Log In');
     $('#login_modal').addClass('active');
     $('#login_title').html('Log In');
     $('#submit_login').removeClass('hide');
     $('#submit_signup').addClass('hide');
     $('#name_input').addClass('hide');
+    $('#login_message_container').addClass('hide');
   });
   $('#clear_login, #close_login').click(function() {
     // Clear modal
     $('#login_modal').removeClass('active');
   });
   $('#clear_login_message').click(function() {
+    // Clear login messages
     $('#login_message_container').addClass('hide');
+  });
+  $('#submit_signup').click(function() {
+    // POST Sign Up
+    $.post('/api/signup', $('#login_form').serialize(), function(data) {
+      console.log(data);
+      if (data.error) {
+        $('#login_message').html(data.message);
+        $('#login_message_container').removeClass('hide');
+      } else {
+        window.location.href = "/";
+      }
+    })
   });
 })
